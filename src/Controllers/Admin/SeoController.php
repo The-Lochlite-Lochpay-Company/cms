1973eb293341b5179b76c2e5fd05750fb004a30a
igmacedom <71357992+igmacedom@users.noreply.github.com>
2022-06-15 00:51:18 -0300

    Add files via upload

---
+++ src/Controllers/Admin/SeoController.php
@@ -0,0 +1,133 @@
+<?php
+/**
+* The Lochlite & Lochpay Company - Technology that makes people's lives easier
+*
+* (c) 2019 - 2022 LOCHLITE E LOCHPAY SOFTWARES E PAGAMENTOS LTDA., All Right Reserved.
+*
+* Software: LOCHLITE CMS
+* Version: 2.0.9  
+* License: Proprietary
+* Made in: Brazil
+* Author: The Lochlite & Lochpay Company
+* Developer: IGOR MACEDO MONTALVÃO
+* Website: https://lochlite.com | https://lochpay.com | https://gpgic.com 
+* Support: drcg@gpgic.com | drcg@lochlite.com | drcl@lochlite.com
+*
+* LEGAL NOTICE: The author(s) of the software grants the user of the software a personal, non-transferable, limited and revocable license without the right to market, resell, distribute, clone or recycle the software; The author(s) reserve the right to renew, revoke or modify the license, as well as impose fines for its violation at its most reasonable discretion.
+*
+* DISCLAIMER: The author(s) of the Software will not be responsible for any physical, moral, property damages or of any nature due to the software, its enjoyment or risks up to the limits of the legislation in force in Brazil.
+*
+* ('Art. 43 - LEI No 4.502/1964' - law of brazil) Indústria Brasileira - LOCHLITE E LOCHPAY SOFTWARES E PAGAMENTOS LTDA, CNPJ: 37.816.728/0001-04; Address: SCS QUADRA 9, BLOCO C, 10 ANDAR, SALA 1003, Brasilia, Federal District, Brazil, Zip Code: 70308-200
+**/
+
+namespace Lochlite\cms\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use Lochlite\cms\Models\Seo;
+
+use Lochlite\cms\Controllers\Controller;
+use Spatie\Permission\Models\Role; use Spatie\Permission\Models\Permission;
+use Lochlitecms; use Carbon\Carbon; use Inertia\Inertia; use Artisan; use Storage; use Config; use DB; use Mail; use Hash; use Route; use Auth; use Arr; use Str;
+
+class SeoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    function __construct()
+    {
+         $this->middleware('auth:sanctum');
+         $this->middleware('permission:publish comments|edit comments|delete comments', ['only' => ['index','show']]);
+         $this->middleware('permission:publish comments', ['only' => ['create','store']]);
+         $this->middleware('permission:edit comments', ['only' => ['edit','update']]);
+         $this->middleware('permission:delete comments', ['only' => ['destroy']]);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+         return Lochlitecms::renderPanelCMS('Panel/seo/index', [
+             'canLogin' => Route::has('login'),
+             'canRegister' => Route::has('register'),
+             'title' => 'Configurações de Search Engine Optimization | Lochlite CMS',
+             'role' => Auth::User()->hasrole(['admin', 'Admin', 'administrador', 'Administrador']) == true ? 'Administrador' : Auth::User()->roles->pluck('name','name')->first() ?? 'Usuário',
+             'avatar' => Auth::User()->avatar ?? '/assets/images/faces-clipart/pic-1.png',
+             'name' => Auth::User()->name ?? 'User Name',
+             'breadcrumbCurrentTitle' => 'Configurações de Search Engine Optimization',
+             'breadcrumbCurrentSection' => 'SEO',
+             'version' => Lochlitecms::application()->get('version'),
+         ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
