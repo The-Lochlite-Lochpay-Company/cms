9a39cae85767c530bca0f625d6ed5e1660e1a38a
igmacedom <71357992+igmacedom@users.noreply.github.com>
2022-07-09 09:17:54 -0300

    Add files via upload

---
+++ src/Controllers/Auth/EmailVerificationPromptController.php
@@ -0,0 +1,44 @@
+<?php
+/**
+* The Lochlite & Lochpay Company - Technology that makes people's lives easier
+*
+* (c) 2019 - 2022 LOCHLITE E LOCHPAY SOFTWARES E PAGAMENTOS LTDA., All Right Reserved.
+*
+* Software: LOCHLITE CMS
+* Version: 2.0.10  
+* License: Proprietary
+* Made in: Brazil
+* Author: The Lochlite & Lochpay Company
+* Developer: IGOR MACEDO MONTALVÃO
+* Website: https://lochlite.com | https://lochpay.com | https://gpgic.com 
+* Support: drcg@gpgic.com | drcg@lochlite.com | drcl@lochlite.com
+*
+* LEGAL NOTICE: The author(s) of the software grants the user of the software a personal, non-transferable, limited and revocable license without the right to market, resell, distribute, clone or recycle the software; The author(s) reserve the right to renew, revoke or modify the license, as well as impose fines for its violation at its most reasonable discretion.
+*
+* DISCLAIMER: The author(s) of the Software will not be responsible for any physical, moral, property damages or of any nature due to the software, its enjoyment or risks up to the limits of the legislation in force in Brazil.
+*
+* ('Art. 43 - LEI No 4.502/1964' - law of brazil) Indústria Brasileira - LOCHLITE E LOCHPAY SOFTWARES E PAGAMENTOS LTDA, CNPJ: 37.816.728/0001-04; Address: SCS QUADRA 9, BLOCO C, 10 ANDAR, SALA 1003, Brasilia, Federal District, Brazil, Zip Code: 70308-200
+**/
+
+namespace Lochlite\cms\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class EmailVerificationPromptController extends Controller
+{
+    /**
+     * Display the email verification prompt.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return mixed
+     */
+    public function __invoke(Request $request)
+    {
+        return $request->user()->hasVerifiedEmail()
+                    ? redirect()->intended(RouteServiceProvider::HOME)
+                    : Inertia::render('Auth/VerifyEmail', ['status' => session('status')]);
+    }
+}
